c519aae93e936709a1019cf3781015d0f11fb31c
apisceva <aljona.gromova@gmail.com>
2022-08-16 06:18:21 -0400

    added to Shopping Cart onIncrement/onDecrement, buyNow button

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -1,36 +1,65 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-    state = {
-        products: [
-            {id: 1, productName: "iPhone", price: 8900, quantity: 0},
-            {id: 2, productName: "Sony Camera", price: 4500, quantity: 0},
-            {id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0},
-            {id: 4, productName: "iPad Pro", price: 12400, quantity: 0},
-            {id: 5, productName: "Xbox", price: 7780, quantity: 0},
-            {id: 6, productName: "Dell Monitor", price: 880, quantity: 0}
-    ]
-    }
+  state = {
+    products: [
+      { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
+      { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
+      { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
+      { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
+      { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
+      { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
+    ],
+  };
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <h4>Shopping Cart</h4>
-        <div>
-            {this.state.products.map((prod) => {
-                return (
-                <Product 
-                key={prod.id} 
-                id={prod.id} 
-                productName={prod.productName} 
-                price={prod.price} 
-                />
-                ); 
-            })}
+
+        <div className="row">
+          {this.state.products.map((prod) => {
+            return (
+              <Product
+                key={prod.id}
+                product={prod}
+                onIncrement={this.handleIncrement}
+                onDecrement={this.handleDecrement}
+              >
+                <button className="btn btn-primary">Buy Now</button>
+              </Product>
+            );
+          })}
         </div>
       </div>
     );
   }
-}
+  //render ends here
 
+   //executes when the user clicks on + button.
+  handleIncrement = (product, maxValue) => {
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity < maxValue) {
+      allProducts[index].quantity++;
+
+      //update the state of current component (parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+
+  //executes when the user clicks on - button.
+  handleDecrement = (product, minValue) => {
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity > minValue) {
+      allProducts[index].quantity--;
+
+      //update the state of current component (parent component)
+      this.setState({products: allProducts})
+    }
+  };
+}
 
