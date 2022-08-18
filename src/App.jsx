695a3e13ba0986b59b18c9c5567f237a500e5019
apisceva <aljona.gromova@gmail.com>
2022-08-18 07:09:56 -0400

    Login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
-import ShoppingCart from "./ShoppingCart";
+import Login from "./Login";
 
-
-
-export default class App extends Component 
-{
-    render()
-    {
-        return (
-            <React.Fragment>
-                <NavBar />
-                <ShoppingCart />
-            </React.Fragment>
-        );       
-    }
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Login />
+      </React.Fragment>
+    );
+  }
 }
